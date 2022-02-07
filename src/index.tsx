6d1e020e034a3df21cc40b562f4e835adef4b7d7
Rz0R <vpa05@mail.ru>
2022-02-07 16:28:07 +1000

    add Redux DevTools to project

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer } from './store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
 import { comments } from './mocks/comments';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
