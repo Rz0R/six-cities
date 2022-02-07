d07c76d8bf770ef0c524deb5f777b2c29a7f8f3d
Rz0R <vpa05@mail.ru>
2022-02-07 16:14:26 +1000

    add redux to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './store/reducer';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
 import { comments } from './mocks/comments';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offerCardsCount={6}
-      offers={offers}
-      comments={comments}
-    />
+    <Provider store={store}>
+      <App
+        offerCardsCount={6}
+        offers={offers}
+        comments={comments}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
