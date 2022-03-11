e3800be376b1e3b7d39a0bbd7e5d2b23a2b2b166
Rz0R <vpa05@mail.ru>
2022-03-11 22:05:43 +1000

    add getFilteredOffers selector

--- src/store/offers-data/selectors.ts
+++ src/store/offers-data/selectors.ts
@@ -1,6 +1,15 @@
+import { createSelector } from 'reselect';
 import { NameSpace } from '../root-reducer';
 import { State } from '../../types/state';
 import { Offers } from '../../types/offer';
+import { getSelectedCity } from '../app-state/selectors';
+import { getSelectedCityOffers } from '../../utils/common';
 
 export const getOffers = (state: State): Offers => state[NameSpace.offers].offers;
 export const getOffersLoadingStatus = (state: State): boolean => state[NameSpace.offers].isOffersDataLoaded;
+
+export const getFilteredOffers = createSelector(
+  getOffers,
+  getSelectedCity,
+  (offers, selectedCity) => getSelectedCityOffers(offers, selectedCity),
+);
