1e9d4e6753a0ae2ab512bcf85dfd10c74f986051
Rz0R <vpa05@mail.ru>
2024-05-21 21:41:12 +1000

    change api src

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 import { getToken } from './token';
 
-const BACKEND_URL = 'https://7.react.pages.academy/six-cities';
-const REQUEST_TIMEOUT = 5000;
+const BACKEND_URL = 'https://11.design.htmlacademy.pro/six-cities';
+const REQUEST_TIMEOUT = 10000;
 
 enum HttpCode {
   Unauthorized = 401,
@@ -27,20 +27,18 @@ export const createAPI = (onUnauthorized: UnauthorizedCallback): AxiosInstance =
       }
 
       return Promise.reject(error);
-    },
+    }
   );
 
-  api.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
-      const token = getToken();
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
 
-      if (token) {
-        config.headers['x-token'] = token;
-      }
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
 
-      return config;
-    },
-  );
+    return config;
+  });
 
   return api;
 };
--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -13,31 +13,32 @@ import {
   setNearbyOffersDataNotFound,
   loadComments,
   setCommentsDataNotFoundStatus,
-  setPostCommentStatus
+  setPostCommentStatus,
 } from './actions';
 import { saveToken, dropToken } from '../services/token';
 import { APIRoute, AuthorizationStatus, PostCommentStatus } from '../const';
-import { BackendOffer } from '../types/offer';
-import { BackendUser } from '../types/user-data';
+import { Offers, Offer } from '../types/offer';
+import { UserData } from '../types/user-data';
 import { AuthData } from '../types/auth-data';
-import { adaptOffersToClient, adaptOfferToClient, adaptUserDataToClient, adaptCommentsDataToClient } from '../services/adapter';
 import { toast } from 'react-toastify';
-import { BackendComments } from '../types/comments';
+import { Comments } from '../types/comments';
 
-const AUTH_FAIL_MESSAGE = 'Don\'t forget to login';
+const AUTH_FAIL_MESSAGE = "Don't forget to login";
 const POST_COMMENT_FAIL_MESSAGE = 'Fail post comment';
 
-export const fetchOfferAction = (): ThunkActionResult =>
+export const fetchOfferAction =
+  (): ThunkActionResult =>
   async (dispatch, _getState, api): Promise<void> => {
-    const { data } = await api.get<BackendOffer[]>(APIRoute.Offers);
-    dispatch(loadOffers(adaptOffersToClient(data)));
+    const { data } = await api.get<Offers>(APIRoute.Offers);
+    dispatch(loadOffers(data));
   };
 
-export const checkAuthAction = (): ThunkActionResult =>
+export const checkAuthAction =
+  (): ThunkActionResult =>
   async (dispatch, _getState, api): Promise<void> => {
-    const { data } = await api.get<BackendUser>(APIRoute.Login);
+    const { data } = await api.get<UserData>(APIRoute.Login);
     if (data) {
-      dispatch(loadUserData(adaptUserDataToClient(data)));
+      dispatch(loadUserData(data));
       dispatch(requireAuthorization(AuthorizationStatus.Auth));
     } else {
       toast.info(AUTH_FAIL_MESSAGE);
@@ -45,62 +46,66 @@ export const checkAuthAction = (): ThunkActionResult =>
     }
   };
 
-export const loginAction = ({ login: email, password }: AuthData): ThunkActionResult =>
+export const loginAction =
+  ({ login: email, password }: AuthData): ThunkActionResult =>
   async (dispatch, _getState, api) => {
-    const { data } = await api.post<BackendUser>(APIRoute.Login, { email, password });
-    saveToken(data.token);
-    dispatch(loadUserData(adaptUserDataToClient(data)));
+    const { data } = await api.post<UserData>(APIRoute.Login, { email, password });
+    saveToken(data?.token || '');
+    dispatch(loadUserData(data));
     dispatch(requireAuthorization(AuthorizationStatus.Auth));
-    const { data: offersData } = await api.get<BackendOffer[]>(APIRoute.Offers);
-    dispatch(loadOffers(adaptOffersToClient(offersData)));
+    const { data: offersData } = await api.get<Offers>(APIRoute.Offers);
+    dispatch(loadOffers(offersData));
   };
 
-export const logoutAction = (): ThunkActionResult =>
-  async (dispatch, _getState, api) => {
-    api.delete(APIRoute.Logout);
-    dropToken();
-    dispatch(removeUserData());
-    dispatch(requireLogout(AuthorizationStatus.NoAuth));
-    const { data: offersData } = await api.get<BackendOffer[]>(APIRoute.Offers);
-    dispatch(loadOffers(adaptOffersToClient(offersData)));
-  };
+export const logoutAction = (): ThunkActionResult => async (dispatch, _getState, api) => {
+  api.delete(APIRoute.Logout);
+  dropToken();
+  dispatch(removeUserData());
+  dispatch(requireLogout(AuthorizationStatus.NoAuth));
+  const { data: offersData } = await api.get<Offers>(APIRoute.Offers);
+  dispatch(loadOffers(offersData));
+};
 
-export const fetchOfferByIdAction = (id: string): ThunkActionResult =>
+export const fetchOfferByIdAction =
+  (id: string): ThunkActionResult =>
   async (dispatch, _getState, api) => {
     try {
-      const { data } = await api.get<BackendOffer>(`${APIRoute.Hotels}/${id}`);
-      dispatch(loadOfferById(adaptOfferToClient(data)));
+      const { data } = await api.get<Offer>(`${APIRoute.Hotels}/${id}`);
+      dispatch(loadOfferById(data));
     } catch {
       dispatch(setCurrentOfferDataNotFoundStatus());
     }
   };
 
-export const fetchNearbyOffersAction = (id: string): ThunkActionResult =>
+export const fetchNearbyOffersAction =
+  (id: string): ThunkActionResult =>
   async (dispatch, _getState, api) => {
     try {
-      const { data } = await api.get<BackendOffer[]>(`${APIRoute.Hotels}/${id}/nearby`);
-      dispatch(loadNearbyOffers(adaptOffersToClient(data)));
+      const { data } = await api.get<Offers>(`${APIRoute.Hotels}/${id}/nearby`);
+      dispatch(loadNearbyOffers(data));
     } catch {
       dispatch(setNearbyOffersDataNotFound());
     }
   };
 
-export const fetchCommentsAction = (id: string): ThunkActionResult =>
+export const fetchCommentsAction =
+  (id: string): ThunkActionResult =>
   async (dispatch, _getState, api) => {
     try {
-      const { data } = await api.get<BackendComments>(`${APIRoute.Comments}/${id}`);
-      dispatch(loadComments(adaptCommentsDataToClient(data)));
+      const { data } = await api.get<Comments>(`${APIRoute.Comments}/${id}`);
+      dispatch(loadComments(data));
     } catch {
       dispatch(setCommentsDataNotFoundStatus());
     }
   };
 
-export const postCommentAction = (id: string, review: { comment: string, rating: string }): ThunkActionResult =>
+export const postCommentAction =
+  (id: string, review: { comment: string; rating: string }): ThunkActionResult =>
   async (dispatch, _getState, api) => {
     try {
       dispatch(setPostCommentStatus(PostCommentStatus.Posting));
-      const res = await api.post<BackendComments>(`${APIRoute.Comments}/${id}`, review);
-      dispatch(loadComments(adaptCommentsDataToClient(res.data)));
+      const { data } = await api.post<Comments>(`${APIRoute.Comments}/${id}`, review);
+      dispatch(loadComments(data));
       dispatch(setPostCommentStatus(PostCommentStatus.Success));
     } catch {
       toast.info(POST_COMMENT_FAIL_MESSAGE);
@@ -108,12 +113,11 @@ export const postCommentAction = (id: string, review: { comment: string, rating:
     }
   };
 
-
-export const toggleIsFavoriteAction = (id: string, favoriteStatus: number): ThunkActionResult =>
+export const toggleIsFavoriteAction =
+  (id: string, favoriteStatus: number): ThunkActionResult =>
   async (dispatch, _getState, api) => {
-    const { data } = await api.post<BackendOffer>(`${APIRoute.Favorite}/${id}/${favoriteStatus}`);
-    const adaptedData = adaptOfferToClient(data);
-    dispatch(updateOffer(adaptedData));
-    dispatch(loadOfferById(adaptedData));
-    dispatch(updateNearbyOffers(adaptedData));
+    const { data } = await api.post<Offer>(`${APIRoute.Favorite}/${id}/${favoriteStatus}`);
+    dispatch(updateOffer(data));
+    dispatch(loadOfferById(data));
+    dispatch(updateNearbyOffers(data));
   };
